03432f05fc6d591899943b96fa533294781dde1b
João Pablo <jp.pablolima@gmail.com>
2026-05-15 07:23:25 -0300

    add models

---
+++ src/models/player-model.ts
@@ -0,0 +1,22 @@
+export interface PlayerModel {
+  id: number;
+  name: string;
+  club: string;
+  nationality: string;
+  position: string;
+  statistics: {
+    Overall: number;
+    Pace: number;
+    Shooting: number;
+    Passing: number;
+    Dribbling: number;
+    Defending: number;
+    Physical: number;
+    Diving: number;
+    Handling: number;
+    Kicking: number;
+    Reflexes: number;
+    Speed: number;
+    Positioning: number;
+  };
+}
